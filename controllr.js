cdd4a71e973df894051934da790ae48c5c077dc9
Leonardo <leonardo.mastrovicz@alunoescolassociais.org.br>
2024-07-04 07:52:11 -0300

    modifiquei meu projeto

--- controllr.js
+++ controllr.js
@@ -1,7 +1,13 @@
 // validação de preenchimento
 function acessar(){
-    let loginEmail = document. getElementById('loginEmail'),
+    let loginEmail = document. getElementById('loginEmail').
     value;
-    let loginsenha = document.getElementById('loginsenha').
+    let loginsenha = document.getElementById('loginSenha').
     value;
+
+    if(!loginEmail || !loginsenha){
+        alert('Favor preencher todos os campos');
+    }else{
+        alert("Campos preenchidos com sucesso");
+    }
 }
