f56166ac1065848320fb8a07f47e4c1c97aa47d1
Leonardo <leonardo.mastrovicz@alunoescolassociais.org.br>
2024-07-04 07:32:18 -0300

    modifiquei o controllr.js

---
+++ controllr.js
@@ -0,0 +1,7 @@
+// validação de preenchimento
+function acessar(){
+    let loginEmail = document. getElementById('loginEmail'),
+    value;
+    let loginsenha = document.getElementById('loginsenha').
+    value;
+}
